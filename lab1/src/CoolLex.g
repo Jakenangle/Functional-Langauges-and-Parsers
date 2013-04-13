0bef892ff9969a479d4e061dbe19586636cfcc1d
Jacob Nangle <jacob@Shawn.(none)>
2013-04-12 21:02:35 -0400

    Inital commit with what is given for lab

---
+++ lab1/src/CoolLex.g
@@ -0,0 +1,52 @@
+/* your name here
+ * CS-435, Spring 2011
+ * an ANTLR lexer grammar for Cool
+ */
+
+lexer grammar CoolLex;
+
+
+/* definitions */
+
+fragment UC_LETTER
+   : 'A' .. 'Z'
+   ;
+
+
+/* keywords */
+
+CLASS : ('c'|'C')('l'|'L')('a'|'A')('s'|'S')('s'|'S') ;
+
+/* keyword objects */
+
+FALSE : 'f' ('a'|'A')('l'|'L')('s'|'S')('e'|'E') ;
+
+/* operators, except for isvoid and not, which are keyword operators */
+
+DOT : '.' ;
+
+/* punctuation */
+
+SEMICOLON : ';' ;
+
+
+/* whitespace */
+
+
+
+/* comments */
+SL_COMMENT :  
+   ;
+
+MULTILINECOMMENT
+  ;
+
+
+/* identifiers */
+
+
+/* integer constant */
+
+
+/* strings */
+
---
+++ lab1/src/CoolLexer.java
@@ -0,0 +1,54 @@
+/* Steven Cater
+ * CS-435, Spring 2011
+ * Driver file for Cool lexer
+ */
+
+/* Do _not_ edit this file. */
+
+import org.antlr.runtime.*;
+import org.antlr.runtime.tree.*;
+import java.io.*;
+import java.util.*;
+
+public class CoolLexer {
+   public static void main(String[] args) throws Exception {
+      IdMap map = new IdMap("CoolLex.tokens");
+      CharStream input = new ANTLRFileStream(args[0]);
+      CoolLex lexer = new CoolLex(input);
+      Token token;
+      while ((token = lexer.nextToken())!=Token.EOF_TOKEN) {
+	 System.out.println("#" + token.getLine()+ 
+			    " " + map.name((token.getType())) +
+			    " |" +token.getText() + "|");
+      }
+   }
+}
+
+
+class IdMap {
+   String [] array = new String [300];
+   
+   IdMap(String fileName) throws IOException {
+      Scanner sc = new Scanner(new File(fileName)).useDelimiter("[=\\s$]+");
+      int index;
+      String val;
+      while(sc.hasNext()) {
+	 val = sc.next();
+	 index = sc.nextInt();
+	 array[index] = val;
+      }
+      sc.close();
+   }
+
+   String name(int index) {
+      if (index < 0) {
+	 return("Bad index in IdMap.name: " + index);
+      }
+      else {
+	 return array[index];
+      }
+      return "Cannot happen";
+   }
+}
+
+      
